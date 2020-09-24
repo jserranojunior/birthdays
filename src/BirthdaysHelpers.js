70dca0070d0db97da9ac7a4f18f4422d0e950145
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2020-09-24 18:17:31 -0300

    Class Birth

--- src/BirthdaysHelpers.js
+++ src/BirthdaysHelpers.js
@@ -1,18 +1,32 @@
 import formatedDates from "./FormatedDates/FormatedDates.js";
-let VerifyBirthdays = {};
+// let VerifyBirthdays = {};
 
-VerifyBirthdays.month = (dataInserida) => {
-  const formatedDate = formatedDates(dataInserida);
+// VerifyBirthdays.month = (dataInserida) => {
+//   const formatedDate = formatedDates(dataInserida);
 
-  const date = new Date();
-  const primeiroDia = new Date(date.getFullYear(), date.getMonth(), 1);
-  const ultimoDia = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+//   const date = new Date();
+//   const primeiroDia = new Date(date.getFullYear(), date.getMonth(), 1);
+//   const ultimoDia = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-  if (formatedDate >= primeiroDia && formatedDate <= ultimoDia) {
-    return true;
-  } else {
-    return false;
+//   if (formatedDate >= primeiroDia && formatedDate <= ultimoDia) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+class VerifyBirthdays {
+  constructor(dateInserted) {
+    this.dateInserted = formatedDates(dateInserted);
+    this.today = new Date();
   }
-};
+  get month() {
+    if (this.dateInserted.getMonth() == this.today.getMonth()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 export default VerifyBirthdays;
--- src/BirthdaysHelpers.test.js
+++ src/BirthdaysHelpers.test.js
@@ -1,37 +1,48 @@
-import VerifyBirthdays from "./BirthdaysHelpers.js";
+import BirthdaysHelpers from "./BirthdaysHelpers.js";
 
 describe("Births of the Month", () => {
   test("Checks whether the date greater than the current month returns false", () => {
     const today = new Date();
-    const nextMonth = today
-      .setMonth(today.getMonth() + 1)
-      .toLocaleString("fr-CA");
-    expect(VerifyBirthdays.month(nextMonth)).toBeFalsy();
+    const nextMonth = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate()
+    );
+    const VerifyBirthdays = new BirthdaysHelpers(nextMonth);
+
+    expect(VerifyBirthdays.month).toBeFalsy();
   });
 
   test("Checks whether the date less than the current month returns false", () => {
     const today = new Date();
-    const previousMonth = today
-      .setMonth(today.getMonth() - 1)
-      .toLocaleString("fr-CA");
-    expect(VerifyBirthdays.month(today)).toBeFalsy();
+    const previousMonth = new Date(
+      today.getFullYear(),
+      today.getMonth() - 1,
+      today.getDate()
+    );
+    const VerifyBirthdays = new BirthdaysHelpers(previousMonth);
+
+    expect(VerifyBirthdays.month).toBeFalsy();
   });
 
   test("Checks whether the current date returns true", () => {
     const today = new Date();
-    expect(VerifyBirthdays.month(today)).toBeTruthy();
+    const VerifyBirthdays = new BirthdaysHelpers(today);
+    expect(VerifyBirthdays.month).toBeTruthy();
   });
 
   test("Checks whether the first day of the month returns true", () => {
     const today = new Date();
-    const secondDay = new Date(today.getFullYear(), today.getMonth(), 2);
-    expect(VerifyBirthdays.month(secondDay)).toBeTruthy();
+    const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+    const VerifyBirthdays = new BirthdaysHelpers(firstDay);
+    expect(VerifyBirthdays.month).toBeTruthy();
   });
 
   test("Checks whether the second day of the month returns true", () => {
     const today = new Date();
-    const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
-    expect(VerifyBirthdays.month(firstDay)).toBeTruthy();
+    const secondDay = new Date(today.getFullYear(), today.getMonth(), 2);
+    const VerifyBirthdays = new BirthdaysHelpers(secondDay);
+    expect(VerifyBirthdays.month).toBeTruthy();
   });
 
   test("Checks whether in pt-br format returns true", () => {
@@ -41,12 +52,23 @@ describe("Births of the Month", () => {
       day: "2-digit",
       year: "numeric",
     });
-    expect(VerifyBirthdays.month(brDay)).toBeTruthy();
+    const VerifyBirthdays = new BirthdaysHelpers(brDay);
+
+    expect(VerifyBirthdays.month).toBeTruthy();
   });
 
   test("Checks whether in US format returns true", () => {
     const today = new Date();
     const usDay = today.toLocaleDateString("fr-ca");
-    expect(VerifyBirthdays.month(usDay)).toBeTruthy();
+    const VerifyBirthdays = new BirthdaysHelpers(usDay);
+    expect(VerifyBirthdays.month).toBeTruthy();
+  });
+
+  test("Checks whether year diferent of birth returns true", () => {
+    const today = new Date();
+    const nextYear = new Date(today.getFullYear() + 1, today.getMonth(), 1);
+    const usDay = nextYear.toLocaleDateString("fr-ca");
+    const VerifyBirthdays = new BirthdaysHelpers(usDay);
+    expect(VerifyBirthdays.month).toBeTruthy();
   });
 });
